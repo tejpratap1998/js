b47a9f66556ea6812a203ab4aa4b48ed834555f3
tejpratap1998 <31912256+tejpratap1998@users.noreply.github.com>
2024-09-21 06:41:21 +0000

    datatypes updates

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 76900
+let accountEmail = "demo@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 77416 //
+
+accountEmail = "demo1@gmail.com"
+accountPassword = "0987654"
+accountCity = "kota"
+
+console.log(accountId);
+
+/*
+
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity])
