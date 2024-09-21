ffe3d71c3be6fa9dc95651dcbce130b229b7a111
tejpratap1998 <31912256+tejpratap1998@users.noreply.github.com>
2024-09-21 08:56:07 +0000

    converstion of datatypes

---
+++ 01_basics/03_converstionoperation.js
@@ -0,0 +1,18 @@
+//let score = "tej"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let isLoggedIn = "tej"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
